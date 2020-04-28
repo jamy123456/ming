33e48391c34b3bfa6655dd0186ec7d63af2f9d8d
jamy123456 <990134943@qq.com>
2020-04-28 22:38:47 +0800

    修改实体类student添加构造方法和get,set方法

--- src/main/java/com/example/ming/entry/Student.java
+++ src/main/java/com/example/ming/entry/Student.java
@@ -6,11 +6,6 @@ import lombok.*;
  * Created by jamy on 2020-04-23
  */
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Setter
-@Getter
-@ToString
 public class Student {
 
     private String name;
@@ -18,4 +13,55 @@ public class Student {
     private int age;
     private String address;
 
+    public Student() {
+    }
+
+    public Student(String name, String sex, int age, String address) {
+        this.name = name;
+        this.sex = sex;
+        this.age = age;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", sex='" + sex + '\'' +
+                ", age=" + age +
+                ", address='" + address + '\'' +
+                '}';
+    }
 }
